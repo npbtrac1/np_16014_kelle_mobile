d388925c3e50578f5a3375dc2fb1024ffb784f09
Lac <lac.phan@enpii.com>
2016-06-06 18:04:57 +0700

    update code

--- www/app/app.component.js
+++ www/app/app.component.js
@@ -12,7 +12,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App</h1>'
+            template: ''
         })
     ], AppComponent);
     return AppComponent;
