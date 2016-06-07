32428506d7e88a240078eddb2ca89cd00681b0b9
Lac <lac.phan@enpii.com>
2016-06-07 18:18:08 +0700

    add angular action for login fomr

--- www/app/app.component.js
+++ www/app/app.component.js
@@ -5,16 +5,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require('@angular/core');
+var router_deprecated_1 = require('@angular/router-deprecated');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Login - Kelle';
+        this.showForm = 0;
+        this.showFormAction = function () {
+            this.showForm = 1;
+            return false;
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: ''
-        })
+            templateUrl: 'app/views/login.component.html',
+            directives: [router_deprecated_1.ROUTER_DIRECTIVES],
+            providers: [
+                router_deprecated_1.ROUTER_PROVIDERS,
+            ],
+        }), 
+        __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
