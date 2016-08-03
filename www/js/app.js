8be8043860533b3771183c3f15b87a2e9388ef43
Lac <lac.phan@enpii.com>
2016-08-03 19:03:17 +0700

    add faclitu screen

--- www/js/app.js
+++ www/js/app.js
@@ -1,104 +1,109 @@
 /**
  * Created by lacphan on 6/27/16.
  */
-(function () {
-    var app = angular.module('app', [
-        'ngRoute',          //$routeProvider
-        'mgcrea.ngStrap',   //bs-navbar, data-match-route directives
-        'controllers'       //Our module frontend/web/js/controllers.js
-    ]);
-    app.config(['$routeProvider', '$httpProvider',
-        function($routeProvider, $httpProvider) {
-            $routeProvider.
-            when('/', {
-                controller: 'HomeController',
-                templateUrl: 'home/home.view.html',
-                controllerAs: 'vm'
-            }).
-            when('/login', {
-                controller: 'LoginController',
-                templateUrl: 'login/login.view.html',
-                controllerAs: 'vm'
-            }).
-            when('/dashboard', {
-                controller: 'DashboardController',
-                templateUrl: 'dashboard/dashboard.view.html',
-                controllerAs: 'vm'
-            }).
-            otherwise({
-                templateUrl: 'partials/404.html'
-            });
-            $httpProvider.interceptors.push('authInterceptor');
-        }
-    ]);
-    app.factory('authInterceptor', function ($q, $window, $location) {
-        return {
-            request: function (config) {
-                if ($window.sessionStorage.access_token) {
-                    //HttpBearerAuth
-                    config.headers.Authorization = 'Bearer ' + $window.sessionStorage.access_token;
-                }
-                return config;
-            },
-            responseError: function (rejection) {
-                if (rejection.status === 401) {
-                    $location.path('/login').replace();
-                }
-                return $q.reject(rejection);
-            }
-        };
-    });
-    app.service('rest', function ($http, $location, $routeParams) {
 
-        return {
+var app = angular.module('app', [
+    'ngRoute',          //$routeProvider
+    'mgcrea.ngStrap',   //bs-navbar, data-match-route directives
+    'controllers'       //Our module frontend/web/js/controllers.js
+]);
 
-            baseUrl: 'http://kelle.don/api/web/v1/',
-            path: undefined,
-
-            models: function () {
-                return $http.get(this.baseUrl + this.path + location.search);
-            },
-
-            model: function () {
-                if ($routeParams.expand != null) {
-                    return $http.get(this.baseUrl + this.path + "/" + $routeParams.id + '?expand=' + $routeParams.expand);
-                }
-                return $http.get(this.baseUrl + this.path + "/" + $routeParams.id);
-            },
-
-            get: function () {
-                return $http.get(this.baseUrl + this.path);
-            },
-
-            postModel: function (model) {
-                return $http.post(this.baseUrl + this.path, model);
-            },
-
-            putModel: function (model) {
-                return $http.put(this.baseUrl + this.path + "/" + $routeParams.id, model);
-            },
-
-            deleteModel: function () {
-                return $http.delete(this.baseUrl + this.path);
-            }
-        };
-
-    });
-
-    app
-        .directive('login', ['$http', function ($http) {
-            return {
-                transclude: true,
-                link: function (scope, element, attrs) {
-                    scope.isGuest = window.sessionStorage._auth == undefined;
-                },
-
-                template: '<a href="login" ng-if="isGuest">Login</a>'
-            }
-        }])
-        .filter('checkmark', function () {
-            return function (input) {
-                return input ? '\u2713' : '\u2718';
-            };
+app.config(['$routeProvider', '$httpProvider',
+    function ($routeProvider, $httpProvider) {
+        $routeProvider.when('/', {
+            controller: 'HomeController',
+            templateUrl: 'home/home.view.html',
+            controllerAs: 'vm'
+        }).when('/login', {
+            controller: 'LoginController',
+            templateUrl: 'login/login.view.html',
+            controllerAs: 'vm'
+        }).when('/dashboard', {
+            controller: 'DashboardController',
+            templateUrl: 'dashboard/dashboard.view.html',
+            controllerAs: 'vm'
+        }).when('/facility/:id', {
+            templateUrl: 'facility/facility.view.html',
+            controller: 'FacilityController'
+        }).when('/task/:id', {
+            templateUrl: 'task/task.view.html',
+            controller: 'TaskController'
+        }).when('/logout', {
+            templateUrl: 'home/home.view.html',
+            controller: 'LogoutController'
+        }).otherwise({
+            templateUrl: 'partials/404.html'
         });
-})();
+        $httpProvider.interceptors.push('authInterceptor');
+    }
+]);
+app.factory('authInterceptor', function ($q, $window, $location) {
+    return {
+        request: function (config) {
+            if ($window.sessionStorage.access_token) {
+                //HttpBearerAuth
+                config.headers.Authorization = 'Bearer ' + $window.sessionStorage.access_token;
+            }
+            return config;
+        },
+        responseError: function (rejection) {
+            if (rejection.status === 401) {
+                $location.path('/login').replace();
+            }
+            return $q.reject(rejection);
+        }
+    };
+});
+app.service('rest', function ($http, $location, $routeParams) {
+
+    return {
+
+        baseUrl: 'http://kelle.don/api/web/v1/',
+        path: undefined,
+
+        models: function () {
+            return $http.get(this.baseUrl + this.path + location.search);
+        },
+
+        model: function () {
+            if ($routeParams.expand != null) {
+                return $http.get(this.baseUrl + this.path + "/" + $routeParams.id + '?expand=' + $routeParams.expand);
+            }
+            return $http.get(this.baseUrl + this.path + "/" + $routeParams.id);
+        },
+
+        get: function () {
+            return $http.get(this.baseUrl + this.path);
+        },
+
+        postModel: function (model) {
+            return $http.post(this.baseUrl + this.path, model);
+        },
+
+        putModel: function (model) {
+            return $http.put(this.baseUrl + this.path + "/" + $routeParams.id, model);
+        },
+
+        deleteModel: function () {
+            return $http.delete(this.baseUrl + this.path);
+        }
+    };
+
+});
+
+app.directive('login', ['$http', function ($http) {
+        return {
+            transclude: true,
+            link: function (scope, element, attrs) {
+                scope.isGuest = window.sessionStorage._auth == undefined;
+            },
+
+            template: '<a href="login" ng-if="isGuest">Login</a>'
+        }
+    }])
+    .filter('checkmark', function () {
+        return function (input) {
+            return input ? '\u2713' : '\u2718';
+        };
+    });
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,9 @@
 var controllers = angular.module('controllers', []);
-controllers.controller('MainController', ['$scope', '$location', '$window',
+var ajaxUrl = 'http://kelle.don/api/web/v1';
+app.controller('MainController', ['$scope', '$location', '$window',
     function ($scope, $location, $window) {
 
-        $scope.loggedIn = function() {
+        $scope.loggedIn = function () {
             return Boolean($window.sessionStorage.access_token);
         };
         $scope.logout = function () {
@@ -10,16 +11,17 @@ controllers.controller('MainController', ['$scope', '$location', '$window',
             $location.path('/login').replace();
         };
     }
-]);/**
+]);
+/**
  * Created by lacphan on 7/31/16.
  */
-controllers.controller('LoginController', ['$scope', '$http', '$window', '$location',
-    function($scope, $http, $window, $location) {
-        var siteUrl = 'http://kelle.don/api/web/v1';
+app.controller('LoginController', ['$scope', '$http', '$window', '$location',
+    function ($scope, $http, $window, $location) {
+
         $scope.login = function () {
             $scope.submitted = true;
             $scope.error = {};
-            $http.post(siteUrl + '/user/login', $scope.userModel).success(
+            $http.post(ajaxUrl + '/user/login', $scope.userModel).success(
                 function (data) {
                     console.log(data);
                     $window.sessionStorage.access_token = data.access_token;
@@ -35,19 +37,187 @@ controllers.controller('LoginController', ['$scope', '$http', '$window', '$locat
         };
     }
 ]);
-controllers.controller('DashboardController', ['$scope', '$http','$window',
-    function ($scope, $http,$window) {
-        var ajaxUrl = 'http://kelle.don/api/web/v1';
-        var buildingController = '/buildings';
+app.controller('DashboardController', ['$scope', '$http', '$window',
+    function ($scope, $http, $window) {
+
+        var buildingController = '';
         var viewAction = '/view-building?id=';
-        building_id =  $window.sessionStorage.building_id;
-        var vm = this;
-        if(building_id != undefined) {
-            jQuery.get( ajaxUrl + buildingController + viewAction + building_id, function( response ) {
-                $scope.building = response;
-                console.log($scope.building);
-            });
+
+        function convertToSlug(Text) {
+            return Text
+                .toLowerCase()
+                .replace(/ /g, '-')
+                .replace(/[^\w-]+/g, '')
+                ;
         }
+
+        building_id = $window.sessionStorage.building_id;
+        if (building_id !== undefined && building_id != null) {
+            jQuery.get(ajaxUrl + '/buildings/view-building?id=' + building_id, function (response) {
+                $scope.building = response;
+
+
+                $('#building-name').html(response.name);
+                blocks = response.building_facilities;
+                blockFilter = '';
+                floorFilter = '';
+                facilityIdFilter = '';
+                blockHtml = '';
+                jQuery.each(blocks, function (index, item) {
+                    blockFilter += '<option value="#block-' + convertToSlug(item.block) + '">' + item.block + '</option>';
+                    floorFilter += '<option value="#floor-' + convertToSlug(item.floor) + '">' + item.floor + '</option>';
+                    facilityIdFilter += '<option value="#facilityID-' + convertToSlug(item.facility_name) + '">' + item.facility_name + '</option>';
+                    blockHtml +=
+                        '<li class="block-item" >' +
+                        '<a href="#/facility/' + item.id + '">' +
+                        '<div class="block-info">' +
+                        '<div class="block-title">' + item.block + '</div>' +
+                        '<div class="block-info-item"><label>Floor: </label>' + item.floor + '</div>' +
+                        '<div class="block-info-item"><label>Facility Type: </label>' + item.facility.facility_type + '</div>' +
+                        '<div class="block-info-item"><label>Facility ID:</label>' + item.facility_name + '</div>' +
+                        '</div>' +
+                        '<div class="block-rating">' +
+                        '<select class="block-rating-star"> ' +
+                        '<option value="1"></option> ' +
+                        '<option value="2"></option> ' +
+                        '<option value="3"></option> ' +
+                        '<option selected value="4"></option> ' +
+                        '<option value="5"></option> ' +
+                        '</select>' +
+                        '<div class="block-rating-number">4.5</div>' +
+                        '<div class="block-rating-label">Ratings</div>' +
+                        '</div>' +
+                        '<div class="clearfix" ></div>' +
+                        '</a>' +
+                        '</li>';
+
+                });
+                $('#block-filter').html(blockFilter);
+                $('#floor-filter').html(floorFilter);
+                $('#facility-filter').html(facilityIdFilter);
+                $('#block-list').html(blockHtml);
+                $('.block-rating-star').barrating({
+                    theme: 'fontawesome-stars',
+                    readonly: true
+                });
+            });
+
+        }
+
 
     }
 ]);
+app.controller('FacilityController', ['$scope','$location', '$window','$routeParams',
+    function ($scope,$location, $window, $routeParams) {
+        jQuery.get(ajaxUrl + '/buildings/view-facility?id=' + $routeParams.id, function (response) {
+            $scope.facility = response;
+            console.log(response);
+
+            $('#building-name').html(response.building.name);
+
+            $('#facility-name').html(response.block);
+            $('#facility-date').html(response.created_at);
+            $('#facility-floor').html(response.floor);
+            $('#facility-type').html(response.facility.facility_type);
+            $('#facility-id').html(response.facility_name);
+            // $('#facility-name').html(response.block);
+            // $('#facility-name').html(response.block);
+
+            tasks = response.tasks;
+
+            tasksHtml = '';
+            jQuery.each(tasks, function (index, item) {
+                tasksHtml +=
+                    '<li class="task-item"> ' +
+                        '<div class="inner"> ' +
+                            '<div class="container"> ' +
+                                '<div class="task-item-title">' +
+                                item.name+
+                                '</div> ' +
+                                '<div class="task-action"> ' +
+                                    '<select class="block-rating-star"> ' +
+                                        '<option value="1"></option> ' +
+                                        '<option value="2"></option> ' +
+                                        '<option value="3"></option> ' +
+                                        '<option selected value="4"></option> ' +
+                                        '<option value="5"></option> ' +
+                                    '</select> ' +
+                                    '<a class="edit-facility" href="#/task/' + item.id + '">Edit</a> ' +
+                                '</div> ' +
+                            '</div> ' +
+                            '<div class="image-control"> ' +
+                                '<a class="image-button btn-image-update" href="#"><i class="fa fa-camera" aria-hidden="true"></i></a> ' +
+                            '</div> ' +
+                        '</div> ' +
+                    '</li>'
+                   ;
+            });
+            $('#task-list').html(tasksHtml);
+
+            $('.block-rating-star').barrating({
+                theme: 'fontawesome-stars',
+                readonly: true
+            });
+        });
+    }
+]);
+app.controller('TaskController', ['$scope','$location', '$window','$routeParams',
+    function ($scope,$location, $window, $routeParams) {
+        jQuery.get(ajaxUrl + '/buildings/view-task?id=' + $routeParams.id, function (response) {
+            $scope.facility = response;
+            console.log(response);
+
+            $('#building-name').html(response.building.name);
+
+            $('#facility-name').html(response.block);
+            $('#facility-date').html(response.created_at);
+            $('#facility-floor').html(response.floor);
+            $('#facility-type').html(response.facility.facility_type);
+            $('#facility-id').html(response.facility_name);
+
+            tasks = response.tasks;
+
+            tasksHtml = '';
+            jQuery.each(tasks, function (index, item) {
+                tasksHtml +=
+                    '<li class="task-item"> ' +
+                    '<div class="inner"> ' +
+                    '<div class="container"> ' +
+                    '<div class="task-item-title">' +
+                    item.name+
+                    '</div> ' +
+                    '<div class="task-action"> ' +
+                    '<select class="block-rating-star"> ' +
+                    '<option value="1"></option> ' +
+                    '<option value="2"></option> ' +
+                    '<option value="3"></option> ' +
+                    '<option selected value="4"></option> ' +
+                    '<option value="5"></option> ' +
+                    '</select> ' +
+                    '<a class="edit-facility" href="#/view-task/' + item.id + '">Edit</a> ' +
+                    '</div> ' +
+                    '</div> ' +
+                    '<div class="image-control"> ' +
+                    '<a class="image-button btn-image-update" href="#"><i class="fa fa-camera" aria-hidden="true"></i></a> ' +
+                    '</div> ' +
+                    '</div> ' +
+                    '</li>'
+                ;
+            });
+            $('#task-list').html(tasksHtml);
+
+            $('.block-rating-star').barrating({
+                theme: 'fontawesome-stars',
+                readonly: true
+            });
+        });
+    }
+]);
+
+app.controller('LogoutController', ['$location', '$window',
+    function ($location, $window) {
+        $window.sessionStorage.clear();
+        $location.path('/');
+    }
+]);
+
