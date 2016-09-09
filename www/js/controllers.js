e1bf6385c64c007ab6cbf8fb105bd39cc635cf7c
Lac <lac.phan@enpii.com>
2016-09-09 09:43:31 +0700

    test cam

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -559,33 +559,6 @@ app.controller('TaskUpdateImageController', ['$scope', '$location', '$window', '
                     }
                 );
             }
-            function uploadPhoto(imageURI) {
-                var options = new FileUploadOptions();
-                options.fileKey="file";
-                options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
-                options.mimeType="image/jpeg";
-
-                var params = new Object();
-                params.value1 = "test";
-                params.value2 = "param";
-
-                options.params = params;
-                options.chunkedMode = false;
-
-                var ft = new FileTransfer();
-                ft.upload(imageURI, "http://yourdomain.com/upload.php", win, fail, options);
-            }
-
-            function win(r) {
-                console.log("Code = " + r.responseCode);
-                console.log("Response = " + r.response);
-                console.log("Sent = " + r.bytesSent);
-                alert(r.response);
-            }
-
-            function fail(error) {
-                console.log(error.code);
-            }
 
             $('#dynamic-form-submit').click(function (e) {
                 e.preventDefault();
