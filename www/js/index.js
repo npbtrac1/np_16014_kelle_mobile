c7998870b49c7c6d2a7af8de108313e2b255c945
Lac <lac.phan@enpii.com>
2016-10-10 17:46:27 +0700

    change mobile header

--- www/js/index.js
+++ www/js/index.js
@@ -27,6 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('pause', enyo.bind(this, this.paused), false);
     },
     // deviceready Event Handler
     //
@@ -36,14 +37,9 @@ var app = {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    receivedEvent: function(id) {},
+    paused: function()
+    {
+        window.localStorage.setItem("laststate", enyo.json.stringify(this.getState()));
     }
 };
